daf673f27e868f59a42def16bd0ab8076e041e71
Satvik Daga <satvik.daga@geminisolutions.in>
2019-06-28 18:27:10 +0530

    Lazy loading done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 import _ from "lodash";
-import printMe from "./print.js";
 import "./style.css";
 
 function component() {
   const element = document.createElement("div");
-  const btn = document.createElement("button");
+  const button = document.createElement("button");
+  const br = document.createElement("br");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  button.innerHTML = "Click me and check the console!";
 
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
+  element.appendChild(br);
+  element.appendChild(button);
 
-  element.appendChild(btn);
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      const print = module.default;
+
+      print();
+    });
 
   return element;
 }
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-if (module.hot) {
-  module.hot.accept("./print.js", function() {
-    console.log("Accepting the updated printMe module!");
-    printMe();
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  });
-}
+
+document.body.appendChild(component());
