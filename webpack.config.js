fd8fd7b30517251d4ead84aaff2831ab04c4f3d7
Satvik Daga <satvik.daga@geminisolutions.in>
2019-06-28 14:17:24 +0530

    Added fonts

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
       }
     ]
   }
