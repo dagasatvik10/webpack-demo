19bc6c0d9395f60217cdd93d3388134a5d131992
Satvik Daga <satvik.daga@geminisolutions.in>
2019-06-28 14:06:10 +0530

    Added image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from "lodash";
-import "./assets/css/style.css";
+import "./style.css";
+import Icon from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.classList.add("icon");
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ["file-loader"]
       }
     ]
   }
